4df6397ee8de7c009ab2595c061e30f961d3bb94
Charith Bhat <charsept04@gmail.com>
2022-10-08 12:23:07 +0530

    base64 feature completed

--- webviews/components/Base64.svelte
+++ webviews/components/Base64.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-
-    let regularText: string =  tsvscode.getState()?.regularText || '';
-    let convertedText: string =  tsvscode.getState()?.convertedText || '';
+    let regularText: string = tsvscode.getState()?.regularText || "";
+    let convertedText: string = tsvscode.getState()?.convertedText || "";
     // $: {
     //     convertedText = regularText.toLocaleUpperCase();
     //     tsvscode.setState({ regularText, convertedText });
@@ -9,41 +8,82 @@
     function onAddingSource(e: any): void {
         regularText = (e.target as HTMLInputElement).value;
         convertedText = btoa(regularText);
-        // convertedText = regularText.toLocaleUpperCase();
-        tsvscode.setState({ regularText, convertedText });  
+        tsvscode.setState({ regularText, convertedText });
     }
 
     function onAddingBase64(e: any): void {
         convertedText = (e.target as HTMLInputElement).value;
-        // regularText = convertedText.toLocaleLowerCase();
         regularText = atob(convertedText);
-        tsvscode.setState({ regularText, convertedText });  
+        tsvscode.setState({ regularText, convertedText });
+    }
+
+    function copyBase64() {
+        let textarea = document.getElementById(
+            "base64-textarea"
+        ) as HTMLInputElement;
+        textarea!.select()!;
+        document.execCommand("copy");
+        document!.getSelection()!.removeAllRanges();
+        document!.getSelection()!.addRange(document.createRange());
+    }
+
+    function copySource() {
+        let textarea = document.getElementById(
+            "source-textarea"
+        ) as HTMLInputElement;
+        textarea!.select()!;
+        document.execCommand("copy");
+        document!.getSelection()!.removeAllRanges();
+        document!.getSelection()!.addRange(document.createRange());
+    }
+
+    function clearRegularText() {
+        regularText = "";
     }
 </script>
 
 <!-- HEY!!!!! If you plan to create a new svelte page. then try adding the below command in your package json as subscript -->
 <!-- --config ./build/node-extension.webpack.config.js  -->
 <div class="padding-for-whole-page">
-    <h1>Base64</h1>
+    <h1>Base64 Encoder / Decoder</h1>
     <br />
 
-    <p class="padding-for-textarea-below">Source</p>
+    <p class="padding-for-textarea-below">Source (Input/Output)</p>
+
+    <button
+        class="short-button margin-for-textarea-below margin-right"
+        on:click={() => {regularText = '';}}>Clear</button
+    >
+    <button class="short-button margin-for-textarea-below" on:click={copySource}
+        >Copy</button
+    >
     <textarea
         placeholder="Add text here"
         name="yo?"
-        id="yolo"
+        id="source-textarea"
         cols="5"
         rows="10"
         type="text"
-        on:input={onAddingSource}
         value={regularText}
+        on:input={onAddingSource}
     />
-    <br>
-    <p class="padding-for-textarea-below">Base64</p>
+    <br />
+
+    <p class="padding-for-textarea-below">Base64 (Input/Output)</p>
+
+    <button
+        class="short-button margin-for-textarea-below margin-right"
+        on:click={() => {convertedText = '';}}>Clear</button
+    >
+
+    <button class="short-button margin-for-textarea-below" on:click={copyBase64}
+        >Copy</button
+    >
+
     <textarea
         placeholder="Add converted text here"
         name="hoe"
-        id="hoehoe"
+        id="base64-textarea"
         cols="30"
         rows="10"
         type="text"
@@ -55,14 +95,25 @@
 <style>
     textarea {
         padding: 10px;
-        outline-style:solid;
+        outline-style: solid;
         outline-color: #191a21;
         outline-width: 1px;
-     }
+    }
     .padding-for-whole-page {
-        margin: 20px;
+        margin: 40px;
+    }
+    .margin-for-textarea-below {
+        margin-bottom: 8px;
     }
     .padding-for-textarea-below {
-        padding-bottom: 5px;
+        padding-top: 10px;
+        display: inline-block;
+    }
+
+    .short-button {
+        width: 60px;
+        margin-left: 10px;
+        float: right;
+        border-radius: 5px;
     }
 </style>
